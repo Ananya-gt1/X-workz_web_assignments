e964422d5be9b6f6ce168e0f7c202e038fc68529
Ananya G T <ananyathippeswamy7353@gmail.com>
2025-04-16 00:41:28 -0700

    updating

---
+++ X-workz css/web.js
@@ -0,0 +1,18 @@
+var age=22
+let gender=Female 
+const institute='x-workz'
+document.writeln(age)
+
+let emp1={id:12,employeeName:'aa',salary:23999}
+
+let emp2=new Object();
+emp2.id=23;
+emp2.employeeName='bb';
+emp2.salary=45999;
+
+function emp3(id,employeeName,salary){
+    this.id=id;
+    this.employeeName=employeeName;
+    this.salary=salary;
+}
+
