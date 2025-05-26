a7e1248deca510b89158d202429fe13629ccdb03
Ananya G T <ananyathippeswamy7353@gmail.com>
2025-05-26 13:00:10 +0530

    updating

--- New/startup.js
+++ New/startup.js
@@ -1,5 +1,17 @@
-var names='abcd'
+
+let emp =new Object();
+emp.id=1;
+emp.employeeName = 'Ani';
+emp.salary=200000;
+emp.id=4;
 
 function functionClick(){
-
-}
+    let pValue=document.getElementsByTagName('p')[0].innerHTML;
+    let paraValue =document.getElementById('para2').innerHTML;
+    let paraName=document.getElementsByName('paraName')[0].innerHTML;
+    let paraClass=document.getElementsByClassName('paraClass')[0].innerHTML;
+console.log(pValue);
+console.log(paraValue);
+console.log(paraName);
+console.log(paraClass);
+} 
