a1a6d07384b212c8c4aff367f78f6f83058330d5
Ananya G T <ananyathippeswamy7353@gmail.com>
2025-06-06 12:30:21 +0530

    updating

--- 05-06/form5.js
+++ 05-06/form5.js
@@ -1,28 +1,36 @@
 function validate() {
     let val = true;
     let names = document.formName.nameName.value;
-    let email = document.formName.emailEmail.value;
-    let password = document.formName.passwordPassword.value;
-    let cpassword = document.formName.confirmPassword.value;
-    let gender = document.formName.genderGender.value;
-    let Course = document.formName.courseCourse.value;
+    // let email = document.formName.emailEmail.value;
+    // let password = document.formName.passwordPassword.value;
+    // let cpassword = document.formName.confirmPassword.value;
+    // let gender = document.formName.genderGender.value;
+    // let Course = document.formName.courseCourse.value;
 
     if (names.length > 20) {
-        alert('the form not submitted')
         val = false;
+        setError('spanNameId', 'Name must be less than 20 characters');
     }
-    else if (!email.includes('@') || !email.includes('.')) {
-        alert('Please enter a valid email address');
-        val = false;
-        }
-    else if (password.length < 8) {
-        alert('Password must be at least 8 characters long');
-        val = false;
+    // else if (!email.includes('@') || !email.includes('.')) {
+    //     alert('Please enter a valid email address');
+    //     val = false;
+    //     }
+    // else if (password.length < 8) {
+    //     alert('Password must be at least 8 characters long');
+    //     val = false;
+    // }
+    // else if (password !== cpassword) {
+    //     alert('Passwords do not match');
+    //     val = false;
+    // }
+    else {
+        alert('Form submitted successfully');
     }
-    else if (password !== cpassword) {
-        alert('Passwords do not match');
-        val = false;
-    }
-    else alert('Form submitted successfully');
     return val;
 }
+
+function setError(id, message) {
+   document.getElementById(id).innerText = message;
+
+}
+    
