4207c55642210e37576514fc15b214f527ed04aa
Ananya G T <ananyathippeswamy7353@gmail.com>
2025-06-06 12:50:22 +0530

    updating

--- 05-06/form5.js
+++ 05-06/form5.js
@@ -1,30 +1,30 @@
 function validate() {
     let val = true;
     let names = document.formName.nameName.value;
-    // let email = document.formName.emailEmail.value;
-    // let password = document.formName.passwordPassword.value;
-    // let cpassword = document.formName.confirmPassword.value;
-    // let gender = document.formName.genderGender.value;
-    // let Course = document.formName.courseCourse.value;
+    let email = document.formName.emailEmail.value;
+    let password = document.formName.passwordPassword.value;
+    let cpassword = document.formName.confirmPassword.value;
+    let gender = document.formName.genderGender.value;
+    let Course = document.formName.courseCourse.value;
 
     if (names.length > 20) {
         val = false;
         setError('spanNameId', 'Name must be less than 20 characters');
     }
-    // else if (!email.includes('@') || !email.includes('.')) {
-    //     alert('Please enter a valid email address');
-    //     val = false;
-    //     }
-    // else if (password.length < 8) {
-    //     alert('Password must be at least 8 characters long');
-    //     val = false;
-    // }
-    // else if (password !== cpassword) {
-    //     alert('Passwords do not match');
-    //     val = false;
-    // }
+    else if (!email.includes('@') || !email.includes('.')) {
+        val = false;
+        setError('spanEmailId', 'Email must contain "@" and "."');
+        }
+    else if (password.length < 8) {
+        val = false;
+        setError('spanPasswordId', 'Password must be at least 8 characters long');
+    }
+    else if (password !== cpassword) {
+        val = false;
+           setError('spanCpasswordId', 'Passwords do not match');
+    }
     else {
-        alert('Form submitted successfully');
+        alert('Form submitted successfully!');
     }
     return val;
 }
@@ -33,4 +33,8 @@ function setError(id, message) {
    document.getElementById(id).innerText = message;
 
 }
-    
+ 
+function clearError(id) {
+   document.getElementById(id).innerText ='';
+
+}
